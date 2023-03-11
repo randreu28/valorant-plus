dca4c084b7600dffc4a2a1027ecfe1a634267ddb
Rubén Chiquin <randreu28@gmail.com>
2023-03-11 17:40:00 +0100

    Quita el boilerplate y hace un fetch inicial a la API

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [agents, setAgents] = useState(null);
+
+  async function getAgents() {
+    const rawData = await fetch("https://valorant-api.com/v1/agents");
+    const data = await rawData.json();
+
+    setAgents(data);
+  }
+  useEffect(() => {
+    getAgents();
+  }, []);
+
+  if (agents === null) {
+    return <p>Cargando...</p>;
+  }
+
+  console.log(agents);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <pre>{JSON.stringify(agents.data[0], null, 2)}</pre>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
