c3cf2760cf5c0311be1ce7cac21a3f0208376508
Rubén Chiquin <randreu28@gmail.com>
2023-04-29 17:39:18 +0200

    Crea getMaps()

--- src/api.js
+++ src/api.js
@@ -1,14 +1,22 @@
 const API_ROOT = "https://valorant-api.com/v1";
 
-export const getAgents = async() => {
-    const response = await fetch(`${API_ROOT}/agents`);
-    const agents = await response.json();
-    console.log(agents.data);
-    return agents.data;
-}
-export const getWeapons = async() => {
-    const response = await fetch(`${API_ROOT}/weapons`);
-    const agents = await response.json();
-    console.log(agents.data);
-    return agents.data;
-}
+export const getAgents = async () => {
+  const response = await fetch(`${API_ROOT}/agents`);
+  const agents = await response.json();
+  console.log(agents.data);
+  return agents.data;
+};
+
+export const getWeapons = async () => {
+  const response = await fetch(`${API_ROOT}/weapons`);
+  const agents = await response.json();
+  console.log(agents.data);
+  return agents.data;
+};
+
+export const getMaps = async () => {
+  const response = await fetch(`${API_ROOT}/maps`);
+  const maps = await response.json();
+  console.log(maps.data);
+  return maps.data;
+};
