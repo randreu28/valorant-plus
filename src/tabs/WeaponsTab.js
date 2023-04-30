b53be2e56a455468f5f995e668644ef450022cc6
CerratoMarc <marc.cerrato.molero@estudiantat.upc.edu>
2023-04-30 22:20:46 +0200

    creo el prototipo del mannequin

--- src/tabs/WeaponsTab.js
+++ src/tabs/WeaponsTab.js
@@ -2,6 +2,7 @@ import FavoriteButton from "../components/FavoriteButton";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View, Text } from "react-native";
 import { colors } from "../lib/colors";
+import Mannequin from "../components/icons/Mannequin";
 
 const Stack = createNativeStackNavigator();
 
@@ -24,6 +25,7 @@ function WeaponsTab1() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ color: "white" }}>Weapons!</Text>
+      <Mannequin head="#ff0000" body="#00ff00" legs="#0000ff"></Mannequin>
     </View>
   );
 }
