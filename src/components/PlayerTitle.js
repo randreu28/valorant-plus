62ac3396bd2efb9755e42df60e729e0e8ea0788e
alexcorhe <alex.coronado@estudiantat.upc.edu>
2023-04-30 18:13:24 +0200

    searcher a nivel visual

---
+++ src/components/PlayerTitle.js
@@ -0,0 +1,25 @@
+import { View, Text } from "react-native";
+import React from "react";
+import FavoriteButton from "../components/FavoriteButton";
+
+export default function PlayerTitle({ children }) {
+  return (
+    <View
+      style={{
+        height: 90,
+        width: "100%",
+        flexDirection: "row",
+        gap: 50,
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 25,
+      }}
+    >
+      <Text style={{ fontSize: 35, color: "white", fontFamily: "tungsten" }}>
+        {children}
+      </Text>
+
+      <FavoriteButton />
+    </View>
+  );
+}
--- src/screens/profile/PlayerTitleScreen.js
+++ src/screens/profile/PlayerTitleScreen.js
@@ -1,45 +1,50 @@
-import React from "react";
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, TextInput } from "react-native";
 import {} from "react-native-web";
 import { FontAwesome } from "@expo/vector-icons";
-
 import { colors } from "../../lib/colors";
+import Title from "../../components/Title";
+import PlayerTitle from "../../components/PlayerTitle";
+import { useNavigation } from "@react-navigation/native";
 
 export default function PlayerTitleScreen() {
-  const [text, onChangeText] = React.useState();
+  const [text, onChangeText] = useState();
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: null,
+    });
+  }, [navigation]);
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: "white", fontSize: 30 }}>Player Title Screen</Text>
+    <View style={{}}>
+      <Title subtitle="PROFILE">PLAYER TITLE</Title>
 
       <View
         style={{
-          flex: 1,
+          borderRadius: 40,
+          backgroundColor: colors.bg,
+          height: 60,
           flexDirection: "row",
-          gap: 10,
-          justifyContent: "center",
+          justifyContent: "space-between",
           alignItems: "center",
+          margin: 20,
+          padding: 20,
         }}
       >
         <TextInput
           placeholder="Search a title..."
           placeholderTextColor="#A5A5A5"
-          style={styles.input}
           onChangeText={onChangeText}
           value={text}
         />
-        <FontAwesome name="search" size={24} color="white" />
+        <FontAwesome name="search" size={20} color="white" />
       </View>
+
+      <PlayerTitle>Catalyst</PlayerTitle>
+      <PlayerTitle>Chicken</PlayerTitle>
+      <PlayerTitle>Foxy</PlayerTitle>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  input: {
-    backgroundColor: colors.bg,
-    flexDirection: "row",
-    color: "A5A5A5",
-    height: 40,
-    margin: 12,
-    padding: 10,
-    borderRadius: 20,
-  },
-});
