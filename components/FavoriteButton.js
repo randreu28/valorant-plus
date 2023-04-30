60027e931452650cc3367ee1969153b1d31b2158
Rubén Chiquin <randreu28@gmail.com>
2023-04-30 11:21:42 +0200

    Elimina los componentes duplicados
    Relacionado con 14d68e8

--- components/FavoriteButton.js
+++
@@ -1,7 +0,0 @@
-import { Text } from "react-native";
-import { colors } from "../lib/colors";
-
-/* TODO */
-export default function FavoriteButton() {
-  return <Text style={{ color: colors.highlights }}>Favorite</Text>;
-}
