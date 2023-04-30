fb393322ac80952647bc88edd396f6aec54aff80
Rubén Chiquin <randreu28@gmail.com>
2023-04-30 13:11:27 +0200

    Corrige un error con la carga de tipografias

--- App.js
+++ App.js
@@ -19,56 +19,57 @@ import { useFonts } from "expo-font";
 const Tab = createBottomTabNavigator();
 
 export default function App() {
-  useFonts({
+  const [fontsHaveLoaded, error] = useFonts({
     // @ts-ignore
     valorant: require("./assets/fonts/valorant.ttf"),
     // @ts-ignore
     tungsten: require("./assets/fonts/tungsten.otf"),
   });
 
-  return (
-    <NavigationContainer
-      theme={{
-        dark: true,
-        colors: {
-          primary: colors.highlights,
-          background: colors.darkBase,
-          card: colors.base,
-          text: colors.neutral,
-          border: colors.darkBase,
-          notification: colors.details,
-        },
-      }}
-    >
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color, size }) => {
-            switch (route.name) {
-              case "Profile":
-                return <ProfileIcon color={color} size={size} />;
-
-              case "Agents":
-                return <AgentsIcon color={color} size={size} />;
-
-              case "Home":
-                return <HomeIcon color={color} size={size} />;
-
-              case "Weapons":
-                return <WeaponsIcon color={color} size={size} />;
-
-              case "Maps":
-                return <MapsIcon color={color} size={size} />;
-            }
+  if (fontsHaveLoaded)
+    return (
+      <NavigationContainer
+        theme={{
+          dark: true,
+          colors: {
+            primary: colors.highlights,
+            background: colors.darkBase,
+            card: colors.base,
+            text: colors.neutral,
+            border: colors.darkBase,
+            notification: colors.details,
           },
-          headerShown: false,
-        })}
+        }}
       >
-        <Tab.Screen name="Profile" component={ProfileTab} />
-        <Tab.Screen name="Agents" component={AgentsTab} />
-        <Tab.Screen name="Home" component={HomeTab} />
-        <Tab.Screen name="Weapons" component={WeaponsTab} />
-        <Tab.Screen name="Maps" component={MapsTab} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              switch (route.name) {
+                case "Profile":
+                  return <ProfileIcon color={color} size={size} />;
+
+                case "Agents":
+                  return <AgentsIcon color={color} size={size} />;
+
+                case "Home":
+                  return <HomeIcon color={color} size={size} />;
+
+                case "Weapons":
+                  return <WeaponsIcon color={color} size={size} />;
+
+                case "Maps":
+                  return <MapsIcon color={color} size={size} />;
+              }
+            },
+            headerShown: false,
+          })}
+        >
+          <Tab.Screen name="Profile" component={ProfileTab} />
+          <Tab.Screen name="Agents" component={AgentsTab} />
+          <Tab.Screen name="Home" component={HomeTab} />
+          <Tab.Screen name="Weapons" component={WeaponsTab} />
+          <Tab.Screen name="Maps" component={MapsTab} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
 }
