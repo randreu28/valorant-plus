46cb72ce9a0a529b9b846049b7a39b000038a569
Rubén Chiquin <randreu28@gmail.com>
2023-04-30 11:36:42 +0200

    Implementa <FavoriteButton/>
    Relacionado con 14d68e8

--- src/components/FavoriteButton.js
+++ src/components/FavoriteButton.js
@@ -1,7 +1,46 @@
-import { Text } from "react-native";
+import React, { useState } from "react";
+import { Text, TouchableHighlight, View } from "react-native";
 import { colors } from "../lib/colors";
+import { AntDesign } from "@expo/vector-icons";
 
-/* TODO */
 export default function FavoriteButton() {
-  return <Text style={{ color: colors.highlights }}>Favorite</Text>;
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  return (
+    <TouchableHighlight
+      onPress={() => setIsFavorite(!isFavorite)}
+      style={{
+        borderRadius: 20,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderColor: colors.highlights,
+        borderWidth: 2,
+        backgroundColor: isFavorite ? colors.highlights : colors.base,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          gap: 10,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <AntDesign
+          name={isFavorite ? "heart" : "hearto"}
+          size={18}
+          color={isFavorite ? colors.darkBase : colors.highlights}
+        />
+        <Text
+          style={{
+            color: isFavorite ? colors.darkBase : colors.highlights,
+            textAlign: "center",
+          }}
+        >
+          Favorite
+        </Text>
+      </View>
+    </TouchableHighlight>
+  );
 }
