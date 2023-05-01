72d49636544e9a51d9ab0e954efa8f018ef6b198
Rubén Chiquin <randreu28@gmail.com>
2023-05-01 10:30:28 +0200

    Corrige el warning de AgentsTab
    Habian nombres repetidos de ruta, lo cual hacían dichos nombres ambiguos a la hora de navegar

--- src/tabs/AgentsTab.js
+++ src/tabs/AgentsTab.js
@@ -1,3 +1,4 @@
+import React from "react";
 import FavoriteButton from "../components/FavoriteButton";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { colors } from "../lib/colors";
@@ -7,7 +8,6 @@ import AgentsDetailScreen from "../screens/agents/AgentsDetailScreen";
 const Stack = createNativeStackNavigator();
 
 export default function AgentsTab() {
-
   return (
     <Stack.Navigator
       screenOptions={{
@@ -18,7 +18,7 @@ export default function AgentsTab() {
         animation: "slide_from_right",
       }}
     >
-      <Stack.Screen name="Agents" component={AgentsScreen} />
+      <Stack.Screen name="AgentsMain" component={AgentsScreen} />
       <Stack.Screen name="AgentsDetails" component={AgentsDetailScreen} />
     </Stack.Navigator>
   );
