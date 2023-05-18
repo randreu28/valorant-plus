37d19f201b2f37cd33f809339a9dcf856d90dc96
jsunyerdev <jordi.lopez.sune@estudiantat.upc.edu>
2023-05-18 19:20:36 +0200

    feat: afegeix colors de fons semi transparent i transparent

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,6 +1,8 @@
 export const colors = {
   base: "#10202F",
   darkBase: "#10202F",
+  darkBaseTransparent: "rgba(16,32,47,0)",
+  darkBaseSemiTransparent: "rgba(16,32,47,0.75)",
   bg:"#0c1823",
   highlights: "#FF4654",
   details: "#22FFC5",
