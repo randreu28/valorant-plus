fb4263884977fe6db35159e0b9bd0ca767aa51ea
Rubén Chiquin <randreu28@gmail.com>
2023-05-20 09:47:14 +0200

    Añade un import que faltaba
    Relacionado con 4118dada

--- src/screens/home/HomeMainScreen.js
+++ src/screens/home/HomeMainScreen.js
@@ -2,13 +2,13 @@ import { View, Text } from "react-native";
 import Slider from "../../components/Slider";
 import React, { useEffect, useState } from "react";
 import { getIntroSlides } from "../../api";
+import Title from "../../components/Title";
 
 export default function HomeMainScreen() {
- 
   return (
-    <View >
-        <Text>Title (header)</Text>
-        <Title isHeader>PAGE TITLE</Title>
-      </View>
+    <View>
+      <Text>Title (header)</Text>
+      <Title isHeader>PAGE TITLE</Title>
+    </View>
   );
 }
