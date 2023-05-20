2c6d8f9176ad0b59e2a79fd16219f9be71babc18
alexcorhe <alex.coronado@estudiantat.upc.edu>
2023-05-20 20:46:17 +0200

    see more button styles

--- src/components/SeeMoreButton.js
+++ src/components/SeeMoreButton.js
@@ -9,35 +9,32 @@ import { AntDesign } from "@expo/vector-icons";
  */
 export default function SeeMoreButton({ onPress }) {
   return (
-    <TouchableHighlight
-      onPress={onPress}
-      style={{
-        borderRadius: 20,
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-        borderColor: colors.highlights,
-        borderWidth: 2,
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          flexDirection: "row",
-          gap: 10,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+    <TouchableHighlight onPress={onPress} style={styles.buttonPress}>
+      <View style={styles.buttonView}>
         <AntDesign name="plus" size={18} color={colors.highlights} />
-        <Text
-          style={{
-            color: colors.highlights,
-            textAlign: "center",
-          }}
-        >
-          See more
-        </Text>
+        <Text style={styles.buttonText}>See more</Text>
       </View>
     </TouchableHighlight>
   );
 }
+
+const styles = StyleSheet.create({
+  buttonPress: {
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderColor: colors.highlights,
+    borderWidth: 2,
+  },
+  buttonView: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: colors.highlights,
+    textAlign: "center",
+  },
+});
