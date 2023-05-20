bbf88673761a2103c17387cca7a736079290065d
alexcorhe <alex.coronado@estudiantat.upc.edu>
2023-05-20 16:27:41 +0200

    searchbar styles

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,35 @@
 import { FontAwesome } from "@expo/vector-icons";
 import React from "react";
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import { colors } from "../lib/colors";
+
 
 export default function SearchBar({ onChangeText }) {
   return (
-    <View
-      style={{
-        borderRadius: 40,
-        backgroundColor: colors.bg,
-        height: 60,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        margin: 20,
-        padding: 20,
-      }}
-    >
+    <View style={styles.searchBar}>
       <TextInput
         placeholder="Search a title..."
         placeholderTextColor="#A5A5A5"
         onChangeText={onChangeText}
-        style={{ color: colors.neutral }}
+        style={styles.searchtext}
       />
       <FontAwesome name="search" size={20} color="white" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  searchBar: {
+    borderRadius: 40,
+    backgroundColor: colors.bg,
+    height: 60,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 20,
+    padding: 20,
+  },
+  searchtext: {
+    color: colors.neutral
+  },
+});
