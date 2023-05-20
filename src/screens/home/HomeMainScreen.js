7374e15c5942b2bef96c71daa6882629ba50e7de
alexcorhe <alex.coronado@estudiantat.upc.edu>
2023-05-20 10:19:11 +0200

    añade titulos a home

--- src/screens/home/HomeMainScreen.js
+++ src/screens/home/HomeMainScreen.js
@@ -3,12 +3,14 @@ import Slider from "../../components/Slider";
 import React, { useEffect, useState } from "react";
 import { getIntroSlides } from "../../api";
 import Title from "../../components/Title";
+import Grid from "../../components/Grid";
 
 export default function HomeMainScreen() {
   return (
     <View>
-      <Text>Title (header)</Text>
-      <Title isHeader>PAGE TITLE</Title>
+      <Title isHeader>FAVORITES</Title>
+      
+      <Title isHeader>DAILY</Title>
     </View>
   );
 }
