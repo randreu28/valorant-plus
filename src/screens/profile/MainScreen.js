6727fb55a402953c3a378764436d5a78e7e535ca
Rubén Chiquin <randreu28@gmail.com>
2023-05-22 10:45:38 +0200

    Feat: Hace que profiile sea 100% height

--- src/screens/profile/MainScreen.js
+++ src/screens/profile/MainScreen.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Title from "../../components/Title";
-
-import { ScrollView, View } from "react-native";
+import { observer } from "mobx-react-lite";
+import { View } from "react-native";
 import PlayerCard from "../../components/PlayerCard";
 import { state } from "../../state";
-import { observer } from "mobx-react-lite";
 
 const MainScreen = () => {
   return (
-    <ScrollView style={{ flex: 1 }}>
+    <>
       <Title isHeader>PROFILE</Title>
-      <View style={{ height: 700 }}>
+      <View style={{ flex: 1 }}>
         <PlayerCard
           playerCard="https://media.valorant-api.com/playercards/fc209787-414b-10d0-dcac-04832fc2c654/largeart.png"
           playerRank="https://media.valorant-api.com/competitivetiers/564d8e28-c226-3180-6285-e48a390db8b1/11/largeicon.png"
@@ -18,8 +17,8 @@ const MainScreen = () => {
           isEditable={state.isEditable()}
         />
       </View>
-    </ScrollView>
+    </>
   );
-}
+};
 
 export default observer(MainScreen);
