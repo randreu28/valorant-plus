1aa98e345b2423165d644a85c2af7275d2af4694
Rubén Chiquin <randreu28@gmail.com>
2023-05-22 10:39:27 +0200

    Feat: Añade scroll en home

--- src/screens/home/HomeMainScreen.js
+++ src/screens/home/HomeMainScreen.js
@@ -1,10 +1,9 @@
-import { View, Text } from "react-native";
-import Slider from "../../components/Slider";
 import React, { useEffect, useState } from "react";
-import { getIntroSlides } from "../../api";
-import Title from "../../components/Title";
-import Grid from "../../components/Grid";
+import { View } from "react-native";
 import { getAgents } from "../../api";
+import Grid from "../../components/Grid";
+import Title from "../../components/Title";
+import { ScrollView } from "react-native";
 
 export default function HomeMainScreen() {
   const [agents, setAgents] = useState(null);
@@ -16,13 +15,15 @@ export default function HomeMainScreen() {
   }, []);
 
   return (
-    <View style={{ height: "100%" }}>
-      <View>
-        <Title isHeader>FAVORITES</Title>
+    <ScrollView>
+      <View style={{ height: "100%" }}>
+        <View>
+          <Title isHeader>FAVORITES</Title>
+        </View>
+        <Grid items={agents} horizontalScroll context="agents" />
+        <Title isHeader>DAILY</Title>
+        <Grid items={agents} horizontalScroll context="agents" />
       </View>
-      <Grid items={agents} horizontalScroll context="agents" />
-      <Title isHeader>DAILY</Title>
-      <Grid items={agents} horizontalScroll context="agents" />
-    </View>
+    </ScrollView>
   );
 }
