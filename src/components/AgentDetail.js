642eaade82933c98c7b3838c22f53a1a451d605c
jsunyerdev <jordi.lopez.sune@estudiantat.upc.edu>
2023-06-10 22:09:24 +0200

    fix: fa el gradient d'agents detail més petit

--- src/components/AgentDetail.js
+++ src/components/AgentDetail.js
@@ -192,9 +192,10 @@ const styles = StyleSheet.create({
   },
   gradient: {
     position: "absolute",
-    top: 0,
+    bottom: 0,
     left: 0,
     right: 0,
     bottom: 0,
+    height: 70,
   },
 });
